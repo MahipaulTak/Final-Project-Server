e4f912f3bd865c1110533d0928b11b2ce914b9f9
mahipaul tak <i.no.u.ok@gmail.com>
2017-04-05 16:24:46 -0600

    added constructor to ticket

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,11 +1,55 @@
 
 /**
- * @author mahipaul.tak, luke.renaud, kevin.widdeman
- * 
  * Aggregated by flight objects and keeps track of passenger info of passenger with flight 
  * and has a price associated
+ * 
+ * @author mahipaul.tak, luke.renaud, kevin.widdeman
+ * 
+ * 
  *
  */
-public class Ticket {
 
+public class Ticket {
+	
+	
+
+
+	/**
+	 * ticket is for this flight
+	 */
+	private Flight fl;
+	
+	/**
+	 * Ticket ID
+	 */
+	private Integer ID;
+	
+	/**
+	 * Info of passenger with ticket
+	 */
+	private PasssengerInfo info;
+	
+	/**
+	 * price of flight including 7% tax
+	 */
+	private Integer price;
+	
+	
+	/**
+	 * Constructor for Ticket
+	 * 
+	 * @param fl
+	 * @param iD
+	 * @param info
+	 * @param price
+	 */
+	public Ticket(Flight fl, Integer iD, PasssengerInfo info, Integer price) {
+		super();
+		this.fl = fl;
+		ID = iD;
+		this.info = info;
+		this.price = price;
+	}
+	
+	
 }
