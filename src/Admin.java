39f553ba4fd38937cd9807b022da2d3ef2f19277
mahipaul tak <i.no.u.ok@gmail.com>
2017-04-05 10:30:18 -0600

    Client update
    Updated with clientside files and moved two projects together into 1

---
+++ src/Admin.java
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ * Gui with additional components required for admin functionality
+ * 
+ * @author mahipaul.tak, luke.renaud, kevin.widdeman
+ * 
+ * 
+ *
+ */
+public class Admin extends Passenger{
+
+}
---
+++ src/Passenger.java
@@ -0,0 +1,12 @@
+
+/**
+ * passenger G.U.I please refer to documentation for requirements 
+ * 
+ * @author mahipaul.tak, luke.renaud, kevin.widdeman
+ * 
+ * 
+ *
+ */
+public class Passenger {
+	
+}
