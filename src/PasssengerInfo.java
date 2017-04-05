b680461a7a5091a3d1b25081cb977b70bb9eee9f
mahipaul tak <i.no.u.ok@gmail.com>
2017-04-05 10:45:38 -0600

    Added class objects to passenger info
    basic passenger info class objects

--- src/PasssengerInfo.java
+++ src/PasssengerInfo.java
@@ -7,5 +7,17 @@
  *
  */
 public class PasssengerInfo {
-
+	/**
+	 * String representing first name of passenger
+	 */
+	private String FName;
+	/**
+	 * String representing last name of passenger
+	 */
+	private String LName;
+	/**
+	 * String representind date of birth of passenger
+	 * in Formtat: DD/MM/YYYY
+	 */
+	private String Bdate;
 }
