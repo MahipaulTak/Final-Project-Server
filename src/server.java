3f3ec0baa42e757f87eb116cdbe75a11b151be9c
mahipaul tak <i.no.u.ok@gmail.com>
2017-04-06 12:23:58 -0600

    added basic constructor
    server uses port 9898

--- src/server.java
+++ src/server.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * @author mahipaul.tak, luke.renaud, kevin.widdeman
@@ -6,5 +12,41 @@
  *
  */
 public class server {
-
+	/**
+	 * Socket of server
+	 */
+	ServerSocket serverSocket;
+	/**
+	 * Socket for interacting with client
+	 */
+	Socket Client;
+	/**
+	 * Reader for input from client
+	 */
+	BufferedReader clientIn;
+	/**
+	 * Output from server to client
+	 */
+	PrintWriter serverOut;
+	/**
+	 * message
+	 */
+	String message;
+	
+	/**
+	 * Constructor for server, connects to port 9898
+	 * 
+	 * @throws IOException
+	 */
+	public server() throws IOException{
+		serverSocket = new ServerSocket(9898,1);
+		System.out.println("Flight Server is now running.");
+		Client = serverSocket.accept();
+		clientIn = new BufferedReader(new InputStreamReader(Client.getInputStream()));
+		serverOut = new PrintWriter(Client.getOutputStream(), true);
+		System.out.println("Server now connected to the client");
+	}
+	
+	
+	
 }
