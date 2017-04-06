3a70453d0103d18bf3987e63840bd527339eb460
mahipaul tak <i.no.u.ok@gmail.com>
2017-04-06 09:24:12 -0600

    fixed flight class with eclipse changes

--- src/Flight.java
+++ src/Flight.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * @author mahipaul.tak, luke.renaud, kevin.widmann
@@ -6,38 +7,41 @@
  * also aggregates tickets
  *
  */
- 
+
 //Probably need some libraries
 public class Flight {
-  public Integer FlightNumber, Duration, NumberOfSeats, RemainingSeats, Price, Time;//Time is just an int of 24 hour time
-  public String, Source, Destination, Date;
-  public ArrayList<Ticket> Tickets;//Check Luke's code on this
-  
-  
-  public Flight(){//default constructor
-    Source = Destination = Date = Time = null;
-    //something something about initializing
-  }
-  
-  public Integer addFlight(Integer duration, Integer numberOfSeats, Integer price, Integer time, String source, String destination, String date){
-    super();
-    remainingSeats = 0;
-    Duration = duration;
-    NumberOfSeats =  numberOfSeats;
-    Price = price;
-    Time = time;
-    Source = source;
-    Destination = destination;
-    Date = date;
-    
-    //add flight to list of flights if there is one
-    //FlightList.add(this);
-    
-    //set flight number to next available number:
-    //flightNumber = getNextFlightNumber();
-    return FlightNumber;
-    
-  }
-  
+	public Integer FlightNumber, Duration, NumberOfSeats, RemainingSeats, Price, Time;//Time is just an int of 24 hour time
+	public String Source, Destination, Date;
+	public ArrayList<Ticket> Tickets;//Check Luke's code on this
+
+
+	public Flight(){//default constructor
+		Source = null;
+		Destination =null;
+		Date = null;
+		Time = null;
+		//something something about initializing
+	}
+
+	public Integer addFlight(Integer duration, Integer numberOfSeats, Integer price, Integer time, String source, String destination, String date){
+
+		RemainingSeats = 0;
+		Duration = duration;
+		NumberOfSeats =  numberOfSeats;
+		Price = price;
+		Time = time;
+		Source = source;
+		Destination = destination;
+		Date = date;
+
+		//add flight to list of flights if there is one
+		//FlightList.add(this);
+
+		//set flight number to next available number:
+		//flightNumber = getNextFlightNumber();
+		return FlightNumber;
+
+	}
+
 
 }
