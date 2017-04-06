816bedb576c6f433d224709e762c45750ba865da
mahipaul tak <i.no.u.ok@gmail.com>
2017-04-06 16:10:11 -0600

    random small changes to database before branch for database

--- src/dbDriver.java
+++ src/dbDriver.java
@@ -1,4 +1,8 @@
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
 /**
  * 
  * Driver for database interactions between flight catalogue and SQL database
@@ -7,5 +11,26 @@
  *
  */
 public class dbDriver {
-
+	
+	
+	/**
+	 * Connection to SQL server
+	 */
+	private Connection conn;
+	/**
+	 * statement being sent to database
+	 */
+	private Statement stmt;
+	/**
+	 * return statement from the server 
+	 */
+	private ResultSet rs;
+	
+	
+	public dbDriver() throws SQLException{
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBASE","root","password");
+		stmt = conn.createStatement();
+	}
+	
+	
 }
