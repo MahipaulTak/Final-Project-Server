7be783302543c01d3f9ae8ddd5e2b388f2ab882c
kwidmann <kevin.widmann@protonmail.com>
2017-04-05 23:54:22 -0600

    Added outline of GUI functionalities

--- src/Passenger.java
+++ src/Passenger.java
@@ -11,6 +11,8 @@ public class Passenger {
 	//TODO - JPanel outer layer?
 	//TODO - other elements we may need access to in the gui
 	
+	private final String ticketDirectory = "./";
+	
 	public Passenger(){
 		make_gui();
 		connect_to_server();
@@ -24,8 +26,18 @@ public class Passenger {
 		//TODO - request flight from server based on flight number
 	}
 	
-	public boolean buy_ticket(Flight requestedFlight){
+	public void searchFlights(){
+		//TODO - get all flights, filter for criteria, display to user
+	}
+	
+	public boolean bookFlight(Flight requestedFlight){
 		//TODO - check if a ticket can be purchased, then alert the server that it has been purchased
+	}
+	
+	public void printTicket(Ticket toPrint){
+		PrintWriter output = new PrintWriter(ticketDirectory+"ticket", "UTF-8");
+		output.println(toPrint.toString());
+		output.close();
 	}
 	
 	private make_gui(){
